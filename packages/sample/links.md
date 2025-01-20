f083da3886132b1a88ee94bcd9379c2fc5c1e6ab
eetann <hideharu092@gmail.com>
2025-01-20 15:49:39 +0900

    feat: definitionで移動

--- packages/sample/links.md
+++ packages/sample/links.md
@@ -5,15 +5,15 @@
 ![image text](test.png)
 
 [local note](grammar.md)
-[local note with heading](grammar.md##List)
+[local note with heading](grammar.md#List)
 
-[current note heading](##wikilink)
+[current note heading](#wikilink)
 
 ## wikilink
 [[https://example.com|example link]] here.
 [[test.png|image text]]
 
 [[grammar.md|local note]]
-[[grammar.md##List|local note with heading]]
+[[grammar.md#List|local note with heading]]
 
-[[##markdown link|current note heading]]
+[[#markdown link|current note heading]]
--- packages/server/src/usecase/provideDefinition/ProvideDefinitionUseCase.ts
+++ packages/server/src/usecase/provideDefinition/ProvideDefinitionUseCase.ts
@@ -1,51 +1,72 @@
+import path from "node:path";
 import type { Index } from "@/domain/model/IndexType";
+import {
+	MarkdownParser,
+	isWikiLinkNode,
+} from "@/domain/service/markdownParser/MarkdownParser";
 import type {
 	CancellationToken,
 	LanguageServicePluginInstance,
 	LocationLink,
 } from "@volar/language-service";
 import type {
+	Range,
 	TextDocument,
 	Position as ZeroBasedPosition,
 } from "vscode-languageserver-textdocument";
+import { URI } from "vscode-uri";
+import { getLineText } from "../shared/utils";
 
 export class ProvideDefinitionUseCase {
-	constructor(private index: Index) {
-		console.log("constructor");
-	}
+	private markdownParser = new MarkdownParser();
+	constructor(private index: Index) {}
 
 	execute: LanguageServicePluginInstance["provideDefinition"] = (
 		textDocument: TextDocument,
-		position: ZeroBasedPosition,
-		token: CancellationToken,
+		cursorPosition: ZeroBasedPosition,
 	) => {
-		// if (token.isCancellationRequested) return;
-		console.log("execute");
-		return [
-			{
-				targetUri: textDocument.uri,
-				targetRange: {
-					start: { line: 0, character: 0 },
-					end: { line: 0, character: 10 },
-				},
-				targetSelectionRange: {
-					start: { line: 0, character: 0 },
-					end: { line: 0, character: 10 },
-				},
-			},
-		];
-		// console.log(token);
-		// // TODO: ここでtokenをパースしてindexから探す
-		// const line = 0;
-		// const range = {
-		// 	start: { line, character: 0 },
-		// 	end: { line, character: 1 },
-		// };
-		// const location: LocationLink = {
-		// 	targetUri: "",
-		// 	targetRange: range,
-		// 	targetSelectionRange: range,
-		// };
-		// return [location];
+		const lineText = getLineText(textDocument, cursorPosition);
+		const oneLinePosition = { ...cursorPosition, line: 0 };
+		const node = this.markdownParser.getCurrentNode(lineText, oneLinePosition);
+
+		if (!isWikiLinkNode(node)) {
+			return [];
+		}
+
+		const [url, heading = ""] = node.value.split("#");
+		if (url.startsWith("http")) {
+			return [];
+		}
+		if (this.index.workspaceFolder === "") {
+			return [];
+		}
+		// TODO: URLが空なら自身の見出しが対象
+		const absolutePath = path.resolve(this.index.workspaceFolder, url);
+		const targetUri = URI.file(absolutePath).toString();
+
+		const doc = this.index.documents[url];
+		if (!doc) {
+			return [];
+		}
+
+		let range: Range = {
+			start: { line: 0, character: 0 },
+			end: { line: 0, character: 0 },
+		};
+		if (heading !== "") {
+			const targetHeading = doc.headings.find((v) =>
+				v.text.startsWith(heading),
+			);
+			if (targetHeading) {
+				range = targetHeading.range;
+			}
+		}
+
+		const location: LocationLink = {
+			targetUri,
+			targetRange: range,
+			targetSelectionRange: range,
+		};
+		return [location];
 	};
 }
